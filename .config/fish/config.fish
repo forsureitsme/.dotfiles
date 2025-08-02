cf276430544f8f66f99586cd406800dd82590db5
Pedro Cardoso da Silva <forsureitsme@gmail.com>
2025-08-02 13:54:37 -0300

    PNPM

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,10 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 function fish_greeting
    # nothing
 end
+
+# pnpm
+set -gx PNPM_HOME "/home/piter/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
--- sh/packages.sh
+++ sh/packages.sh
@@ -8,6 +8,6 @@ printf '\e[A\e[K' # Erase line of animation
 
 echo Install script dependencies
 BLA::start_loading_animation "${BLA_modern_metro[@]}"
-paru -S nodejs webp-pixbuf-loader keyd spotify mc lazygit bluetuith-bin zen-browser-bin vscodium --noconfirm --quiet --print | sed 1,2d >/dev/null
+paru -S nodejs pnpm webp-pixbuf-loader keyd spotify mc lazygit bluetuith-bin zen-browser-bin vscodium --noconfirm --quiet --print | sed 1,2d >/dev/null
 BLA::stop_loading_animation
 printf '\e[A\e[K' # Erase line of animation
